1f3ddb7b5e31cb0e8484c307421455ebd2f6a972
Michael <michaelcbreuer@gmail.com>
2020-02-26 14:41:55 +0800

    adding quiz class

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,1 +1,14 @@
 import React, { Component } from "react";
+
+quizData = require("./quiz_data.json");
+
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div />;
+  }
+}
+
+export default Quiz;
